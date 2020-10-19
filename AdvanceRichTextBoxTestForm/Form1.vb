25d17af0035957ebef95e9790cdaa3dbad994dfa
Paul M Cohen <paul1956@users.noreply.github.com>
2020-10-19 15:29:45 -0700

    Add license files

--- AdvanceRichTextBoxTestForm/Form1.vb
+++ AdvanceRichTextBoxTestForm/Form1.vb
@@ -1,4 +1,8 @@
-﻿Imports AdvanceRichTextBox
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports AdvanceRichTextBox
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
