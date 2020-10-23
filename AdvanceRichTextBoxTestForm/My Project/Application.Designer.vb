1089af1ead8058efc346ac03a8ff42a0a4c5f0ee
Paul M Cohen <paul1956@users.noreply.github.com>
2020-10-23 09:36:08 -0700

    Add splashScheen for Demo

--- AdvanceRichTextBoxTestForm/My Project/Application.Designer.vb
+++ AdvanceRichTextBoxTestForm/My Project/Application.Designer.vb
@@ -13,26 +13,32 @@ Option Explicit On
 
 
 Namespace My
-
+    
     'NOTE: This file is auto-generated; do not modify it directly.  To make changes,
     ' or if you encounter build errors in this file, go to the Project Designer
     ' (go to Project Properties or double-click the My Project node in
     ' Solution Explorer), and make changes on the Application tab.
     '
     Partial Friend Class MyApplication
-
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Public Sub New()
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-            Me.IsSingleInstance = False
+            Me.IsSingleInstance = false
             Me.EnableVisualStyles = True
             Me.SaveMySettingsOnExit = True
+            Me.MinimumSplashScreenDisplayTime = 5000
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
         End Sub
-
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Form1
+            Me.MainForm = Global.WinFormsTestForm.Form1
+        End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.WinFormsTestForm.SplashScreen1
         End Sub
     End Class
 End Namespace
